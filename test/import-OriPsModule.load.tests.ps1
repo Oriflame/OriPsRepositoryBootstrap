8f086a86aacd82f286d22764c1fe16ac849db6ff
Matej Pokorny <mpokorny@oriflame.com>
2020-02-13 14:10:07 +0100

    Update unit tests

--- test/import-OriPsModule.load.tests.ps1
+++ test/import-OriPsModule.load.tests.ps1
@@ -3,8 +3,8 @@ Import-Module -Name "$PSScriptRoot\..\src\PsRepositoryBootstrap.psd1";
 
 Describe 'Testing Import-OriPsModule' {
     InModuleScope PsRepositoryBootstrap {
-        $numCalls          = @{ "0.9.0" = 0; "1.0.0" = 0; "1.1.0" = 0 };
-        $numCallsUntilTrue = @{ "0.9.0" = 0; "1.0.0" = 1; "1.1.0" = 2 };
+        $numCalls          = @{ [Version]"0.9.0" = 0; [Version]"1.0.0" = 0; [Version]"1.1.0" = 0 };
+        $numCallsUntilTrue = @{ [Version]"0.9.0" = 0; [Version]"1.0.0" = 1; [Version]"1.1.0" = 2 };
 
         Mock Test-GetModule { 
             if($numCallsUntilTrue[$RequiredVersion] -ne $numCalls[$RequiredVersion]) {
@@ -13,56 +13,78 @@ Describe 'Testing Import-OriPsModule' {
             }
 
             return $true;
-        }
-        Mock Import-Module {}
-        Mock Install-Module {}
-        Mock Invoke-RegisterOriflameFeeds {}
-
-        It "Parameter validation: Missing [Name] and [RequiredVersion]" {
-            Import-OriPsModule | Should Throw;
-        }
-
-        It "Parameter validation: Missing [RequiredVersion]" {
-            Import-OriPsModule -Name "someModule" | Should Throw;
-        }
+        };
+        Mock Import-Module {};
+        Mock Install-Module {};
+        Mock Invoke-RegisterOriflameFeeds {};
 
         It "Module is already imported" {
-            Import-OriPsModule -Name "someModule" -RequiredVersion "0.9.0" -Verbose;
-            Assert-MockCalled -CommandName Test-GetModule -Times 1 -Exactly;
-            Assert-MockCalled -CommandName Import-Module -Times 0 -Exactly;
-            Assert-MockCalled -CommandName Install-Module -Times 0 -Exactly;
-            Assert-MockCalled -CommandName Invoke-RegisterOriflameFeeds -Times 0 -Exactly;
+            Import-OriPsModule -Name "someModule" -RequiredVersion "0.9.0";
+            Assert-MockCalled -CommandName Test-GetModule -Times 1 -Exactly -Scope It;
+            Assert-MockCalled -CommandName Import-Module -Times 0 -Exactly -Scope It;
+            Assert-MockCalled -CommandName Install-Module -Times 0 -Exactly -Scope It;
+            Assert-MockCalled -CommandName Invoke-RegisterOriflameFeeds -Times 0 -Exactly -Scope It;
         } 
 
         It "Module needs to be imported" {
-            Import-OriPsModule -Name "someModule" -RequiredVersion "1.0.0" -Verbose;
-            Assert-MockCalled -CommandName Test-GetModule -Times 1 -Exactly;
-            Assert-MockCalled -CommandName Import-Module -Times 1 -Exactly;
-            Assert-MockCalled -CommandName Install-Module -Times 0 -Exactly;
-            Assert-MockCalled -CommandName Invoke-RegisterOriflameFeeds -Times 0 -Exactly;
+            Import-OriPsModule -Name "someModule" -RequiredVersion "1.0.0";
+            Assert-MockCalled -CommandName Test-GetModule -Times 2 -Exactly -Scope It;
+            Assert-MockCalled -CommandName Import-Module -Times 1 -Exactly -Scope It;
+            Assert-MockCalled -CommandName Install-Module -Times 0 -Exactly -Scope It;
+            Assert-MockCalled -CommandName Invoke-RegisterOriflameFeeds -Times 0 -Exactly -Scope It;
         }
 
         It "Module needs to be installed" {
-            Import-OriPsModule -Name "someModule" -RequiredVersion "1.1.0" -Verbose;
-            Assert-MockCalled -CommandName Test-GetModule -Times 2 -Exactly;
-            Assert-MockCalled -CommandName Import-Module -Times 1 -Exactly;
-            Assert-MockCalled -CommandName Install-Module -Times 1 -Exactly;
-            Assert-MockCalled -CommandName Invoke-RegisterOriflameFeeds -Times 1 -Exactly;
+            Import-OriPsModule -Name "someModule" -RequiredVersion "1.1.0";
+            Assert-MockCalled -CommandName Test-GetModule -Times 2 -Exactly -Scope It;
+            Assert-MockCalled -CommandName Import-Module -Times 2 -Exactly -Scope It;
+            Assert-MockCalled -CommandName Install-Module -Times 1 -Exactly -Scope It;
+            Assert-MockCalled -CommandName Invoke-RegisterOriflameFeeds -Times 2 -Exactly -Scope It;
         }
     }
 }
 
 Describe 'Testing Test-GetModule' {
     InModuleScope PsRepositoryBootstrap {
-        Mock Get-Module { return $true; } -ParameterFilter { $RequiredVersion -and $RequiredVersion -eq "0.9.0" };
-        Mock Get-Module { return $false; } -ParameterFilter { $RequiredVersion -and $RequiredVersion -eq "1.0.0" };
+        Mock Get-Module { return @{ Version = [Version]"1.0.0" }; } -ParameterFilter { $Name -and $Name -eq "ImportedModule" };
+        Mock Get-Module { return @{ Version = [Version]"0.9.0" }; } -ParameterFilter { $Name -and $Name -eq "NotImportedModule" };
 
         It "Module is already imported" {
-            Test-GetModule -Name "someModule" -RequiredVersion "0.9.0" -Verbose | Should -Be $true;
+            Test-GetModule -Name "ImportedModule" -RequiredVersion "0.9.0" | Should -Be $true;
         }
 
         It "Module is not yet imported" {
-            Test-GetModule -Name "someModule" -RequiredVersion "1.0.0" -Verbose | Should -Be $false;
+            Test-GetModule -Name "NotImportedModule" -RequiredVersion "1.0.0" | Should -Be $false;
+        }
+    }
+}
+
+Describe 'Testing Invoke-RegisterOriflameFeeds' {
+    InModuleScope PsRepositoryBootstrap {
+        Context "No registered package feeds" {
+            It "PowerShellGet is not installed" {
+                Mock Test-GetModule { return $false; };
+                Mock Install-Module;
+                Mock Get-PSRepository { return @(); };
+
+                Invoke-RegisterOriflameFeeds -Feeds @();
+                Assert-MockCalled -CommandName Test-GetModule -Times 1 -Exactly;
+                Assert-MockCalled -CommandName Install-Module -Times 1 -Exactly;
+                Assert-MockCalled -CommandName Get-PSRepository -Times 1 -Exactly;
+            }
+        }
+
+        Context "Twp registered package feed" {
+            It "One of the package feed is not registered" {
+                Mock Test-GetModule { return $true; };
+                Mock Get-PSRepository { return @([PSCustomObject]@{ SourceLocation = "https://pkgs.dev.azure.com/oriflame/_packaging/PackageManagementFeed/nuget/v2" }) };
+                Mock Register-PSRepository {};
+
+                Invoke-RegisterOriflameFeeds -Feeds @( "GlobalDev" );
+                Assert-MockCalled -CommandName Test-GetModule -Times 1 -Exactly;
+                Assert-MockCalled -CommandName Get-PSRepository -Times 1 -Exactly;
+                Assert-MockCalled -CommandName Register-PSRepository -Times 1 -Exactly;
+            }
         }
     }
 }
